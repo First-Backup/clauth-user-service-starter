e341888927151d0352b1bd9a9d7fa86e4d914e4c
DeveloperPrince <prince.maposa@ciibo.com>
2021-02-21 06:47:59 +0200

    released version 4.3.7

--- src/main/java/com/corelogic/clauth/starters/userservice/autocomplete/AutocompleteClient.java
+++
@@ -1,7 +0,0 @@
-package com.corelogic.clauth.starters.userservice.autocomplete;
-
-import com.corelogic.clauth.starters.userservice.autocomplete.dtos.AutocompleteResponse;
-
-public interface AutocompleteClient {
-    AutocompleteResponse search(String input, String applicationCode);
-}
